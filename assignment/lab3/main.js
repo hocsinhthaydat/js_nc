867dd88a1aa956ffc32111e938f78f602fef221f
abc <accpoly14805@gmail.com>
2022-01-13 12:59:56 +0700

    up

--- assignment/lab3/main.js
+++ assignment/lab3/main.js
@@ -1,20 +1,24 @@
 import Navigo from "navigo";
-import menu from "./pages/menu";
+import header from "./pages/header";
+import home from "./pages/home";
 import signin from "./pages/signin";
 
 
 const router = new Navigo("/", { linksSelector: "a" });
 
 const print = (content) => {
-    document.getElementById("menu").innerHTML = menu.render();
+    document.getElementById("menu").innerHTML = header.render();
     document.getElementById("lab3").innerHTML = content;
 }
 router.on({
     "/": () => {
-        print(menu.render());
+        print(home.render());
     },
     "/signin": () => {
         print(signin.render());
     },
+    "/signup": () => {
+        print()
+    }
 });
 router.resolve();
